fae9067c23d577c98bed540cfb4eb8ee4eaeb211
Lukas Steinbrecher <lukas.steinbrecher@senacor.com>
2021-01-14 23:47:36 +0100

    k

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,56 @@ use petgraph::graph::NodeIndex;
 use std::ops::IndexMut;
 use piston::Position;
 use crate::ui::PosF;
+use std::fmt::{Display, Formatter};
+use std::fmt;
+use crate::ConnectorDirection::{Output, Input};
 
 mod ui;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum ConnectorDirection {
+    Input,
+    Output,
+}
+
+#[derive(Debug, Clone)]
 struct Connector {
-    name: String
+    name: String,
+    direction: ConnectorDirection,
 }
 
 impl Connector {
-    fn new(name: &str) -> Connector {
+    fn new_output(name: String) -> Connector {
+        Self::new(name, Output)
+    }
+    fn new_input(name: String) -> Connector {
+        Self::new(name, Input)
+    }
+    fn new(name: String, direction: ConnectorDirection) -> Connector {
         Connector {
-            name: name.into()
+            name,
+            direction,
         }
+    }
+}
+
+impl Display for Connector {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "({})", self.name)
     }
 }
 
 impl PartialEq for Connector {
     fn eq(&self, other: &Self) -> bool {
-        self.name == other.name
+        self.name == other.name && self.direction == other.direction
     }
 }
 
 #[derive(Debug)]
 struct FunctionBox {
     name: String,
-    inputs: Vec<String>,
-    outputs: Vec<String>,
+    inputs: Vec<Connector>,
+    outputs: Vec<Connector>,
 
     position: PosF,
 }
@@ -37,8 +60,15 @@ struct FunctionBox {
 impl FunctionBox {
     fn new(name: &str, position: PosF, inputs: Vec<String>, outputs: Vec<String>) -> FunctionBox {
         FunctionBox {
-            name: name.into(), inputs, outputs, position
+            name: name.into(),
+            inputs: inputs.into_iter().map(|n| Connector::new(n, ConnectorDirection::Input)).collect(),
+            outputs: outputs.into_iter().map(|n| Connector::new(n, ConnectorDirection::Output)).collect(),
+            position,
         }
+    }
+
+    fn connectors(&self) -> impl Iterator<Item=&Connector> {
+        self.inputs.iter().chain(self.outputs.iter())
     }
 }
 
@@ -60,15 +90,17 @@ impl Container {
         self.graph.add_node(function_box)
     }
 
-    fn connect(&mut self, output_ref: FunctionBoxRef, output_connector: String, input_ref: FunctionBoxRef, input_connector: String,) {
+    fn connect(&mut self, output_ref: FunctionBoxRef, output_connector: Connector, input_ref: FunctionBoxRef, input_connector: Connector) {
         assert!(self.graph[output_ref].outputs.contains(&output_connector), "unknown output {}", output_connector);
         assert!(self.graph[input_ref].inputs.contains(&input_connector), "unknown input {}", input_connector);
+        assert!(matches!(output_connector.direction, ConnectorDirection::Output), "wrong direction {}", output_connector);
+        assert!(matches!(input_connector.direction, ConnectorDirection::Input), "wrong direction {}", input_connector);
         assert!(self.graph.first_edge(input_ref, Direction::Incoming).is_none(), "input {} already connected", input_connector);
 
         let edge_ref = self.graph.find_edge(output_ref, input_ref).unwrap_or(self.graph.add_edge(output_ref, input_ref, Vec::new()));
         let vec = self.graph.index_mut(edge_ref);
 
-        let new_edge = (Connector::new(&output_connector), Connector::new(&input_connector));
+        let new_edge = (output_connector, input_connector);
         if !vec.contains(&new_edge) {
             vec.push(new_edge)
         }
--- src/ui.rs
+++ src/ui.rs
@@ -13,15 +13,22 @@ use piston::event_loop::*;
 
 use glutin_window::GlutinWindow as AppWindow;
 use std::f64::consts::PI;
-use crate::{FunctionBox, Container};
+use crate::{FunctionBox, Container, Connector, ConnectorDirection};
 use std::cmp::max;
 use self::graphics::{Rectangle, color, CircleArc};
 use self::graphics::rectangle::{square, Border};
 use self::graphics::types::{Color, Radius};
-use self::piston::Position;
+use self::piston::{Position, Size};
 use self::graphics::math::Vec2d;
 use std::cell::RefCell;
 use std::ptr;
+use std::ptr::eq;
+use petgraph::graph::{Node, NodeIndex};
+use std::ops::IndexMut;
+use vecmath::{vec2_add, vec2_sub};
+use crate::ConnectorDirection::Output;
+use std::mem::replace;
+
 
 pub type PosF = Vec2d;
 type OurGraphics = GlGraphics;
@@ -32,9 +39,16 @@ struct DrawCtx<'a> {
     window: &'a dyn Window,
 }
 
-struct State<'a> {
+#[derive(Debug)]
+struct State {
+    mouse_position: PosF,
+    mouse_button1_pressed: bool,
+    mouse_delta: PosF,
+    window_size: Size,
+
     container: Container,
-    highlighted_function_box: Option<&'a FunctionBox>
+    dragged_function_box: Option<(NodeIndex, PosF)>,
+    dragged_connector: Option<(NodeIndex, Connector, PosF)>,
 }
 
 pub fn ui_main() {
@@ -43,23 +57,32 @@ pub fn ui_main() {
         .exit_on_esc(true).graphics_api(opengl).build().unwrap();
 
     let ref mut gl = GlGraphics::new(opengl);
-    let mut cursor = [0.0, 0.0];
-    let mut window_size = window.draw_size();
 
     let mut state = State {
         container: Container::new(),
-        highlighted_function_box: None
+        mouse_button1_pressed: false,
+        mouse_position: [0., 0.],
+        mouse_delta: [0., 0.],
+        window_size: Size { width: 0., height: 0. },
+        dragged_function_box: None,
+        dragged_connector: None,
     };
     let and_box = state.container.add(FunctionBox::new("and", [50., 50.], vec!["i1".into(), "i2".into()], vec!["and".into()]));
-    let one_box = state.container.add(FunctionBox::new("1",  [50., 200.], vec![], vec!["1".into()]));
-    state.container.connect(one_box, "1".into(), and_box, "i1".into());
+    let one_box = state.container.add(FunctionBox::new("1", [50., 200.], vec![], vec!["1".into()]));
+    state.container.connect(one_box, Connector::new_output("1".into()), and_box, Connector::new_input("i1".into()));
 
-    state.highlighted_function_box = Some(&state.container.graph[and_box]);
+    let mut mouse_position = state.mouse_position;
+    let mut mouse_delta = state.mouse_delta;
+    let mut mouse_button1_pressed = state.mouse_button1_pressed;
+    let mut window_size = state.window_size;
 
     let mut events = Events::new(EventSettings::new().lazy(true));
     while let Some(e) = events.next(&mut window) {
         if let Some(Button::Mouse(button)) = e.press_args() {
             println!("Pressed mouse button '{:?}'", button);
+            if let MouseButton::Left = button {
+                mouse_button1_pressed = true;
+            }
         }
         if let Some(Button::Keyboard(key)) = e.press_args() {
             println!("Pressed keyboard key '{:?}'", key);
@@ -70,16 +93,24 @@ pub fn ui_main() {
         if let Some(button) = e.release_args() {
             match button {
                 Button::Keyboard(key) => println!("Released keyboard key '{:?}'", key),
-                Button::Mouse(button) => println!("Released mouse button '{:?}'", button),
+                Button::Mouse(button) => {
+                    println!("Released mouse button '{:?}'", button);
+                    if let MouseButton::Left = button {
+                        mouse_button1_pressed = false;
+                    }
+                }
                 Button::Controller(button) => println!("Released controller button '{:?}'", button),
                 Button::Hat(hat) => println!("Released controller hat `{:?}`", hat),
             }
         };
         e.mouse_cursor(|pos| {
-            cursor = pos;
             println!("Mouse moved '{} {}'", pos[0], pos[1]);
+            mouse_position = pos;
         });
-        e.mouse_relative(|d| println!("Relative mouse moved '{} {}'", d[0], d[1]));
+        e.mouse_relative(|d| {
+            println!("Relative mouse moved '{} {}'", d[0], d[1]);
+            mouse_delta = d;
+        });
         e.resize(|args| {
             println!("Resized '{}, {}'", args.window_size[0], args.window_size[1]);
             window_size = args.draw_size.into();
@@ -89,6 +120,12 @@ pub fn ui_main() {
         };
         if let Some(args) = e.render_args() {
             // println!("Render {}", args.ext_dt);
+
+            state.window_size = window_size;
+            state.mouse_position = mouse_position;
+            state.mouse_button1_pressed = mouse_button1_pressed;
+            state.mouse_delta = mouse_delta;
+
             gl.draw(args.viewport(), |c, g| {
                 graphics::clear(rgba(178, 190, 195, 1.0), g);
                 let mut ctx = DrawCtx {
@@ -96,7 +133,7 @@ pub fn ui_main() {
                     c: &c,
                     window: &window,
                 };
-                draw(&state, &mut ctx);
+                draw(&mut state, &mut ctx);
             },
             );
         }
@@ -109,18 +146,52 @@ fn rgba(r: i32, g: i32, b: i32, a: f32) -> Color {
     [r as f32 / 255., g as f32 / 255., b as f32 / 255., a]
 }
 
+
 fn draw(
-    state: &State,
-    ctx: &mut DrawCtx
+    state: &mut State,
+    ctx: &mut DrawCtx,
 ) {
-    state.container.graph.raw_nodes().iter().map(|x| {
-        let mut draw = FunctionBoxDraw::new(&x.weight);
-        if let Some(highlighted) = state.highlighted_function_box {
-            draw.highlighted = ptr::eq(highlighted, &x.weight);
+    if !state.mouse_button1_pressed {
+        state.dragged_function_box = None;
+    }
+
+    state.container.graph.node_indices().for_each(|i| {
+        let mut draw = FunctionBoxDraw::new(&state.container.graph[i]);
+
+        if state.mouse_button1_pressed {
+            if let Some((fb, hpos)) = state.dragged_function_box {
+                if eq(draw.function_box, &state.container.graph[fb]) {
+                    drop(draw);
+
+                    let pos = &mut state.container.graph.index_mut(i).position;
+                    *pos = vec2_sub(state.mouse_position, hpos);
+                    println!("Position {:?}", pos);
+
+                    draw = FunctionBoxDraw::new(&state.container.graph[i]);
+                    draw.highlighted = true;
+                }
+            } /*else if let Some((fb, conn, hpos)) = state.dragged_connector.clone() {
+                if eq(draw.function_box, &state.container.graph[fb]) {
+                    draw.highlighted_connector = Some(conn);
+                }
+            } else {
+                let origin = vec2_sub(state.mouse_position, draw.function_box.position);
+                match draw.collide(state.mouse_position) {
+                    Some(FunctionBoxCollideDesc::FunctionBox) => {
+                        state.dragged_function_box = Some((i, origin));
+                    }
+                    Some(FunctionBoxCollideDesc::Connector(connector)) => {
+                        state.dragged_connector = Some((i, connector, origin));
+                    }
+                    _ => {}
+                }
+            }*/
+        } else {
+            state.dragged_function_box = None;
         }
-        draw
-    }).for_each(|d| {
-        d.draw(ctx)
+
+
+        draw.draw(ctx)
     })
 }
 
@@ -131,7 +202,7 @@ struct FunctionBoxDraw<'a> {
     connector_radius: f64,
     connector_margin: f64,
     highlighted: bool,
-
+    highlighted_connector: Option<Connector>,
 }
 
 impl<'a> FunctionBoxDraw<'a> {
@@ -141,7 +212,7 @@ impl<'a> FunctionBoxDraw<'a> {
         let connector_margin = 8.;
 
         let height = 2. * padding + 40.;
-        let width = 2. * padding - connector_margin + ((connector_margin + connector_radius*2.) * max(function_box.outputs.len(), function_box.inputs.len()) as f64);
+        let width = 2. * padding - connector_margin + ((connector_margin + connector_radius * 2.) * max(function_box.outputs.len(), function_box.inputs.len()) as f64);
         let rect = [function_box.position[0], function_box.position[1], width, height];
 
         FunctionBoxDraw {
@@ -150,8 +221,24 @@ impl<'a> FunctionBoxDraw<'a> {
             padding,
             connector_radius,
             connector_margin,
-            highlighted: false
+            highlighted: false,
+            highlighted_connector: None,
         }
+    }
+
+    fn connector_position(&self, connector: &Connector) -> PosF {
+        let cs = if matches!(connector.direction, ConnectorDirection::Input) { &self.function_box.inputs } else { &self.function_box.outputs };
+        let i = cs.iter().position(|x| x == connector).unwrap();
+        self.connector_position_idx(i, connector.direction)
+    }
+
+    fn connector_position_idx(&self, connector_idx: usize, connector_direction: ConnectorDirection) -> PosF {
+        let i = connector_idx as f64;
+
+        [
+            self.rect[0] + self.padding + (i * self.connector_margin + i * self.connector_radius * 2. + self.connector_radius),
+            self.rect[1] + (self.rect[3] * (if matches!(connector_direction, ConnectorDirection::Input) { 1. } else { 0. }))
+        ]
     }
 }
 
@@ -166,44 +253,55 @@ trait Draw {
 
 enum FunctionBoxCollideDesc {
     FunctionBox,
-    Connector(String)
+    Connector(Connector),
 }
 
 impl Collide for FunctionBoxDraw<'_> {
     type CollideDesc = FunctionBoxCollideDesc;
 
     fn collide(&self, point: [f64; 2]) -> Option<FunctionBoxCollideDesc> {
-        None
+        let [x1, y1] = point;
+
+        if let Some((_i, conn)) =
+        self.function_box.outputs.iter().enumerate()
+            .chain(self.function_box.inputs.iter().enumerate())
+            .find(|(i, x)| {
+                let [x2, y2] = self.connector_position_idx(*i, x.direction);
+
+                f64::sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) <= self.connector_radius
+            }) {
+            Some(FunctionBoxCollideDesc::Connector(conn.clone()))
+        } else if x1 >= self.rect[0]
+            && x1 <= self.rect[0] + self.rect[2]
+            && y1 >= self.rect[1]
+            && y1 <= self.rect[1] + self.rect[3] {
+            Some(FunctionBoxCollideDesc::FunctionBox)
+        } else {
+            None
+        }
     }
 }
 
 impl Draw for FunctionBoxDraw<'_> {
     fn draw(&self, ctx: &mut DrawCtx) {
-        let bg_color = if self.highlighted { rgba(253, 203, 110,1.0) } else { rgba(9, 132, 227, 1.0) };
+        let bg_color = if self.highlighted { rgba(253, 203, 110, 1.0) } else { rgba(9, 132, 227, 1.0) };
         let mut rectangle = Rectangle::new_round_border(bg_color, 3., 0.);
         rectangle = rectangle.color(bg_color);
         rectangle.draw_tri(self.rect, &Default::default(), ctx.c.transform, ctx.g);
 
         let fb = self.function_box;
-
-        let mut draw_arc = |i, y| {
-            draw_arc_centered([
-                                  fb.position[0] + self.padding + (i*self.connector_margin + i*self.connector_radius*2. + self.connector_radius),
-                                  y],
-                              self.connector_radius, rgba(99, 110, 114, 1.0), ctx);
-        };
-
         // draw connectors
-        fb.outputs.iter().enumerate().for_each(|(i, name)| {
-            draw_arc(i as f64, fb.position[1]);
-        });
-        fb.inputs.iter().enumerate().for_each(|(i, name)| {
-            draw_arc(i as f64, fb.position[1] + self.rect[3]);
-        });
+        fb.outputs.iter().enumerate()
+            .chain(fb.inputs.iter().enumerate())
+            .for_each(|(i, c)| {
+                let highlighted = self.highlighted_connector.as_ref().map_or(false, |x| { *x == *c });
+                draw_arc_centered(self.connector_position_idx(i, c.direction),
+                                  self.connector_radius, rgba(99, 110, 114, 1.0), highlighted, ctx);
+            });
     }
 }
 
-fn draw_arc_centered(center: PosF, circle_radius: Radius, color: Color, ctx: &mut DrawCtx) {
-    CircleArc::new(color, circle_radius/2., 0., 2.*PI)
-        .draw_tri([center[0] - circle_radius, center[1] - circle_radius, circle_radius*2., circle_radius*2.], &Default::default(), ctx.c.transform, ctx.g);
+fn draw_arc_centered(center: PosF, circle_radius: Radius, color: Color, highlighted: bool, ctx: &mut DrawCtx) {
+    CircleArc::new(color, circle_radius / if highlighted { 1. } else { 2. }, 0., 2. * PI)
+        .draw_tri([center[0] - circle_radius, center[1] - circle_radius, circle_radius * 2., circle_radius * 2.], &Default::default(), ctx.c.transform, ctx.g);
 }
