0f89c48285fd6db0a5f68163c2dd92b96e9bd4ea
Lukas Steinbrecher <lukas.steinbrecher@senacor.com>
2021-01-16 12:35:57 +0100

    sa

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use crate::piston_example::main_piston;
+
 mod ui;
 mod game;
 mod function_box_draw;
-
+mod piston_example;
 
 fn main() {
-    ui::ui_main();
+    main_piston();
+    //ui::ui_main();
 }
