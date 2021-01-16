72058c1b5b06bcece7c4b4994778a2be9f6e5181
Lukas Steinbrecher <lukas.steinbrecher@senacor.com>
2021-01-16 12:36:04 +0100

    Revert "sa"
    This reverts commit 0f89c48285fd6db0a5f68163c2dd92b96e9bd4ea.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use crate::piston_example::main_piston;
-
 mod ui;
 mod game;
 mod function_box_draw;
-mod piston_example;
+
 
 fn main() {
-    main_piston();
-    //ui::ui_main();
+    ui::ui_main();
 }
